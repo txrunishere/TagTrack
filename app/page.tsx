86bd327f87493ee06d292e377381108416ea7a5e
txrunishere <tsoni9286@gmail.com>
2026-04-11 23:19:04 +0530

    feat: add footer message to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
     <div className="min-h-screen bg-linear-to-br from-blue-50 via-white to-blue-100">
       <Header user={user} />
       <HomeSection user={user} />
+      <p className="text-center text-neutral-400">Made by TARUN with ❤️</p>
     </div>
   );
 }
